85a029d2517e79cedb3de4cc7c5f2dc0d188beee
Artem <artemzakharchuk@gmail.com>
2023-05-02 09:24:47 +0300

    one more try

--- server/src/app.js
+++ server/src/app.js
@@ -18,19 +18,17 @@ app.use("/v1", api);
 
 app.get("/v1/*", (req, res) => {
   const path = `/v1/${api}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
-app.get('/v1/:slug', (req, res) => {
+app.get("/v1/:slug", (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
 
 app.get("/*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "client", "build", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 module.exports = app;
