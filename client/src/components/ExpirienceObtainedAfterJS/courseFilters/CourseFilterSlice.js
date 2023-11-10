c1f3e4310838d99156103a5aa2474ace9c34b5d2
Artem Zakharchuk <artemzakharchuk@gmail.com>
2023-11-11 00:03:39 +0200

    Update CourseFilterSlice.js

--- client/src/components/ExpirienceObtainedAfterJS/courseFilters/CourseFilterSlice.js
+++ client/src/components/ExpirienceObtainedAfterJS/courseFilters/CourseFilterSlice.js
@@ -11,9 +11,9 @@ export const filtersFetch = createAsyncThunk(
   'filters/filtersFetch',
   async () => {
     const { request } = useHttp();
-    // return request('https://cv-back-362t.onrender.com/v1/courses');
+    return request('https://cv-back-362t.onrender.com/v1/courses');
     // return request("https://jagged-elated-penguin.glitch.me/filters");
-    return await request("http://localhost:8000/v1/courses");
+    // return await request("http://localhost:8000/v1/courses");
   }
 );
 
